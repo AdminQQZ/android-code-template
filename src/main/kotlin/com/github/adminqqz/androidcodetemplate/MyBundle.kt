f69e6aca9beaa316041d97438d25fe418cb02ec8
GitHub Action <action@github.com>
2022-08-03 07:03:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/adminqqz/androidcodetemplate/MyBundle.kt
+++ src/main/kotlin/com/github/adminqqz/androidcodetemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.adminqqz.androidcodetemplate
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/adminqqz/androidcodetemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.adminqqz.androidcodetemplate.services
+
+import com.github.adminqqz.androidcodetemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
